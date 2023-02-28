1c11e1f6db94cc233aed0cca9b7249d390ad4325
tre3p <whereareyo@yahoo.com>
2023-02-28 16:06:33 +0300

    add logs to storage

--- src/main/java/com/treep/storage/RouteDefinitionStorage.java
+++ src/main/java/com/treep/storage/RouteDefinitionStorage.java
@@ -1,19 +1,24 @@
 package com.treep.storage;
 
 import com.treep.config.model.RouteDefinition;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 
+@Slf4j
 public class RouteDefinitionStorage {
 
     private static Map<String, RouteDefinition> routeDefinitionStorage;
 
     public static void setRouteDefinitionStorage(Map<String, RouteDefinition> storage) {
+        log.debug("setRouteDefinitionStorage(): storage size: {}", storage.size());
         routeDefinitionStorage = storage;
     }
 
     public static RouteDefinition getRouteDefinitionBySourceEndpoint(String sourceEndpoint) {
-        RouteDefinition result = routeDefinitionStorage.getOrDefault(sourceEndpoint, null);
+        log.debug("+getRouteDefinitionBySourceEndpoint(): looking for endpoint: {}", sourceEndpoint);
+        RouteDefinition result = routeDefinitionStorage.get(sourceEndpoint);
+        log.debug("-getRouteDefinitionBySourceEndpoint(): result: {}", result);
         return result;
     }
 
