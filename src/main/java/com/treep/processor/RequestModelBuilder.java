d1a1b4126c071f83016f144c58caf91ec9b56ec9
tre3p <whereareyo@yahoo.com>
2023-03-01 22:49:17 +0300

    add HttpRequest builder

---
+++ src/main/java/com/treep/processor/RequestModelBuilder.java
@@ -0,0 +1,80 @@
+package com.treep.processor;
+
+import com.treep.exception.RequestBuildingException;
+import com.treep.model.GatewayModel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpRequest;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+
+import static com.treep.util.ErrorConstants.ERROR_BUILDING_REQUEST_MODEL;
+
+@Slf4j
+public class RequestModelBuilder {
+
+    public static HttpRequest buildHttpRequestModel(GatewayModel requestModel) {
+        log.debug("+buildRequestModel()");
+        String url = buildUrl(requestModel);
+        Map<String, String> requestHeaders = requestModel.getRequestHeaders();
+        byte[] requestBody = requestModel.getRequestBody();
+        int requestTimeout = requestModel.getRequestTimeout();
+        URI builtUrl;
+
+        try {
+            builtUrl = new URI(url);
+        } catch (URISyntaxException e) {
+            log.error("-buildHttpRequestModel(): error while building request model", e);
+            throw new RequestBuildingException(ERROR_BUILDING_REQUEST_MODEL, e);
+        }
+
+        HttpRequest.Builder req = HttpRequest.newBuilder();
+        req.uri(builtUrl);
+        injectHeader(requestHeaders, req);
+        determineAndInjectRequestMethod(req, requestModel.getHttpMethod(), requestBody);
+        injectTimeout(req, requestTimeout);
+
+        HttpRequest builtRequest = req.build();
+        log.debug("-buildHttpRequestModel()");
+        return builtRequest;
+    }
+
+    private static void injectHeader(Map<String, String> headers, HttpRequest.Builder builder) {
+        log.debug("injectHeader()");
+        headers.forEach((headerName, headerValue) -> builder.setHeader(headerName, headerValue));
+    }
+
+    private static String buildUrl(GatewayModel requestModel) {
+        log.debug("+buildUrl()");
+        String host = requestModel.getTargetHost();
+
+        if (requestModel.getTargetQueryParams() != null && !requestModel.getTargetQueryParams().isEmpty()) {
+            host += "?" + requestModel.getTargetQueryParams();
+        }
+
+        log.debug("-buildUrl()");
+        return host;
+    }
+
+    private static void determineAndInjectRequestMethod(
+            HttpRequest.Builder requestBuilder,
+            String requestMethod, byte[] requestBody)
+    {
+        log.debug("+determineAndInjectRequestMethod():");
+        switch (requestMethod.toUpperCase()) {
+            case "GET" -> requestBuilder.GET();
+            case "POST" -> requestBuilder.POST(HttpRequest.BodyPublishers.ofByteArray(requestBody));
+            case "PUT" -> requestBuilder.PUT(HttpRequest.BodyPublishers.ofByteArray(requestBody));
+            case "DELETE" -> requestBuilder.DELETE();
+        }
+        log.debug("-determineAndInjectRequestMethod()");
+    }
+
+    private static void injectTimeout(HttpRequest.Builder builder, int timeout) {
+        log.debug("injectTimeout()");
+        builder.timeout(Duration.of(timeout, ChronoUnit.SECONDS));
+    }
+}
