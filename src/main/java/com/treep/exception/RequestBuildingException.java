b2ef4cbafa95d6ea2041c01755ba3e5ca39b3a1a
tre3p <whereareyo@yahoo.com>
2023-03-01 22:48:52 +0300

    add exceptions for request building and request execution

---
+++ src/main/java/com/treep/exception/RequestBuildingException.java
@@ -0,0 +1,8 @@
+package com.treep.exception;
+
+public class RequestBuildingException extends RuntimeException {
+
+    public RequestBuildingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
