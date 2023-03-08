8f170d6507ade31ada56e98e6f39d8e7ba9788ef
tre3p <whereareyo@yahoo.com>
2023-03-08 15:32:40 +0300

    add abstract test to reduce boilerplate annotations

---
+++ src/test/java/integration/WiremockAbstractTest.java
@@ -0,0 +1,25 @@
+package integration;
+
+import com.treep.exception.ConfigurationReadingException;
+import com.treep.exception.RoutesValidationException;
+import com.treep.util.constants.ConfigConstants;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
+import util.Utils;
+
+import java.io.IOException;
+
+import static util.TestConstants.TEST_GATEWAY_CONFIG_PATH;
+
+@SetEnvironmentVariable.SetEnvironmentVariables({
+        @SetEnvironmentVariable(key = ConfigConstants.GATEWAY_CONFIG_LOCATION_ENV, value = TEST_GATEWAY_CONFIG_PATH)
+})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public abstract class WiremockAbstractTest {
+
+    @BeforeAll
+    void beforeAll() throws ConfigurationReadingException, RoutesValidationException, IOException {
+        Utils.startServer();
+    }
+}
