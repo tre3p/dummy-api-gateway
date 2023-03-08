c6db743e137935fd8958f0e1945d9f59f2fb61e9
tre3p <whereareyo@yahoo.com>
2023-03-08 16:42:02 +0300

    remove unused imports

--- src/test/java/integration/WiremockAbstractTest.java
+++ src/test/java/integration/WiremockAbstractTest.java
@@ -1,17 +1,12 @@
 package integration;
 
 import com.treep.Main;
-import com.treep.exception.ConfigurationReadingException;
-import com.treep.exception.RoutesValidationException;
 import com.treep.util.constants.ConfigConstants;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junitpioneer.jupiter.SetEnvironmentVariable;
-import util.Utils;
-
-import java.io.IOException;
 
 import static util.TestConstants.TEST_GATEWAY_CONFIG_PATH;
 
