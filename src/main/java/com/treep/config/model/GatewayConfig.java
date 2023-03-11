34524e0a95e1b4100612974711b29142632d1aee
tre3p <whereareyo@yahoo.com>
2023-03-11 16:58:00 +0300

    add tests for GatewayConfigValidator

--- src/main/java/com/treep/config/model/GatewayConfig.java
+++ src/main/java/com/treep/config/model/GatewayConfig.java
@@ -2,9 +2,11 @@ package com.treep.config.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GatewayConfig {
 
     private String serverPort;
