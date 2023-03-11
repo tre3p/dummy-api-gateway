5e70445b69e1d17ae9eda02bb6eed1e28f450045
tre3p <whereareyo@yahoo.com>
2023-03-11 16:28:54 +0300

    add tests for server thread count reading from env

--- src/test/java/unit/config/GatewayConfigReaderTest.java
+++ src/test/java/unit/config/GatewayConfigReaderTest.java
@@ -2,10 +2,14 @@ package unit.config;
 
 import com.treep.config.GatewayConfigReader;
 import com.treep.config.model.GatewayConfig;
-import com.treep.util.constants.ConfigConstants;
 import org.junit.jupiter.api.Test;
 import org.junitpioneer.jupiter.SetEnvironmentVariable;
 
+import static com.treep.util.constants.ConfigConstants.DEFAULT_GATEWAY_CONFIG_LOCATION;
+import static com.treep.util.constants.ConfigConstants.DEFAULT_SERVER_PORT;
+import static com.treep.util.constants.ConfigConstants.GATEWAY_CONFIG_LOCATION_ENV;
+import static com.treep.util.constants.ConfigConstants.SERVER_PORT_ENV;
+import static com.treep.util.constants.ConfigConstants.SERVER_THREAD_COUNT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -15,16 +19,18 @@ class GatewayConfigReaderTest {
 
     private static final String EXPECTED_CONFIG_FILE_NAME = "test/location/file.yml";
 
+    private static final String EXPECTED_THREAD_COUNT = "100";
+
     @Test
     void shouldCorrectlySetDefaultsOnNoEnv() {
         GatewayConfig gatewayConfig = GatewayConfigReader.readEnv();
 
-        assertEquals(ConfigConstants.DEFAULT_SERVER_PORT, gatewayConfig.getServerPort());
-        assertEquals(ConfigConstants.DEFAULT_GATEWAY_CONFIG_LOCATION, gatewayConfig.getConfigLocation());
+        assertEquals(DEFAULT_SERVER_PORT, gatewayConfig.getServerPort());
+        assertEquals(DEFAULT_GATEWAY_CONFIG_LOCATION, gatewayConfig.getConfigLocation());
     }
 
     @Test
-    @SetEnvironmentVariable(key = ConfigConstants.SERVER_PORT_ENV, value = EXPECTED_PORT)
+    @SetEnvironmentVariable(key = SERVER_PORT_ENV, value = EXPECTED_PORT)
     void shouldCorrectlyReadPortFromEnv() {
         GatewayConfig actualConfig = GatewayConfigReader.readEnv();
 
@@ -32,7 +38,7 @@ class GatewayConfigReaderTest {
     }
 
     @Test
-    @SetEnvironmentVariable(key = ConfigConstants.GATEWAY_CONFIG_LOCATION_ENV, value = EXPECTED_CONFIG_FILE_NAME)
+    @SetEnvironmentVariable(key = GATEWAY_CONFIG_LOCATION_ENV, value = EXPECTED_CONFIG_FILE_NAME)
     void shouldCorrectlyReadFileLocationFromEnv() {
         GatewayConfig actualConfig = GatewayConfigReader.readEnv();
 
@@ -40,14 +46,24 @@ class GatewayConfigReaderTest {
     }
 
     @Test
+    @SetEnvironmentVariable(key = SERVER_THREAD_COUNT, value = EXPECTED_THREAD_COUNT)
+    void shouldCorrectlyReadThreadCountFromEnv() {
+        GatewayConfig actualConfig = GatewayConfigReader.readEnv();
+
+        assertEquals(EXPECTED_THREAD_COUNT, actualConfig.getServerThreadCount());
+    }
+
+    @Test
     @SetEnvironmentVariable.SetEnvironmentVariables({
-            @SetEnvironmentVariable(key = ConfigConstants.GATEWAY_CONFIG_LOCATION_ENV, value = EXPECTED_CONFIG_FILE_NAME),
-            @SetEnvironmentVariable(key = ConfigConstants.SERVER_PORT_ENV, value = EXPECTED_PORT)
+            @SetEnvironmentVariable(key = GATEWAY_CONFIG_LOCATION_ENV, value = EXPECTED_CONFIG_FILE_NAME),
+            @SetEnvironmentVariable(key = SERVER_PORT_ENV, value = EXPECTED_PORT),
+            @SetEnvironmentVariable(key = SERVER_THREAD_COUNT, value = EXPECTED_THREAD_COUNT)
     })
-    void shouldCorrectlyReadServerPortAndFileNameFromEnv() {
+    void shouldCorrectlyReadAllConfigurationsFromEnv() {
         GatewayConfig actualConfig = GatewayConfigReader.readEnv();
 
         assertEquals(EXPECTED_CONFIG_FILE_NAME, actualConfig.getConfigLocation());
         assertEquals(EXPECTED_PORT, actualConfig.getServerPort());
+        assertEquals(EXPECTED_THREAD_COUNT, actualConfig.getServerThreadCount());
     }
 }
